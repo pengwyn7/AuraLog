e3cbb5a87315f9b94e03258ef274390dd0f62464
pengwyn7 <gwyns9703@gmail.com>
2025-10-19 03:18:15 +0800

    feature(AuraLog): complete app setup

--- AuraLog_project/AuraLog_app/models.py
+++ AuraLog_project/AuraLog_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class MoodEntry(models.Model):
+    date = models.DateField(default=date.today, unique=True)
+    mood_score = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.date} - Mood {self.mood_score}"
